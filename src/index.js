a5fcc8bb4efc55ea791c7df67a72be5ed9aa7952
Anuska Sinha <119009678+anuskasinha1222@users.noreply.github.com>
2023-10-08 23:17:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+
+// my map
+
+
+
+function myMap(fn){
+  
+  let self = this;
+  res = [];
+  console.log(self,"selgf")
+  
+  for(let i=0; i<self.length; i++){
+    res[i]= fn(self[i],i,self)
+  }
+  return res
+  
+}
+
+Array.prototype.myMap = myMap;
+
+const newData = [1,2,3,4,5,6].myMap((ele,index)=>{
+  return ele*2
+})
+console.log(newData,"newData")
